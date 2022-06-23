0a405b24c0095082d2848e9a21e6e3437d5c09f5
Kashaf Ansari <kashafansari3108@gmail.com>
2022-06-23 19:37:09 +0500

    Assignment Done

---
+++ app.js
@@ -0,0 +1,506 @@
+let mobiles = {
+    samsung:{
+        A53:{
+            modelName:"Samsung Galaxy A53",
+            priceInRs:85999,
+            priceIn$:641,
+            batteryCapacity:`(Li-Po Non removable), 5000 mAh -
+             Fast charging 33W, 100% in 65 min (advertised)`, 
+            audio: "Speaker Phone",
+            sizeInInches: 6.5,
+            colors:['Black','White','Blue','Peach'],
+            sim:"Hybrid Dual SIM, Dual Standby, (Nano-SIM)",
+            rating:2.3,
+            image:"./img/samsung.png"
+        },
+        A23:{
+            modelName:"Samsung Galaxy A23",
+            priceInRs:48500,
+            priceIn$:241,
+            batteryCapacity:`(Li-Po Non removable), 5000 mAh  
+            - Fast charging 25W`, 
+            audio: "3.5mm Audio Jack, Speaker Phone",
+            sizeInInches: 6.6,
+            colors:['Black','White','Blue','Peach'],
+            sim:"Hybrid Dual SIM, Dual Standby, (Nano-SIM)",
+            rating:4.5,
+            image:"./img/samsung.png"
+        },
+        A33:{
+            modelName:"Samsung Galaxy A33",
+            priceInRs: 71500,
+            priceIn$:355,
+            batteryCapacity:`(Li-Po Non removable), 5000 mAh  
+            - Fast charging 25W`, 
+            audio: "3.5mm Audio Jack, Speaker Phone",
+            sizeInInches: 6.4,
+            colors:['Black','White','Blue','Peach'],
+            sim:"Hybrid Dual SIM, Dual Standby, (Nano-SIM)",
+            rating:3.2,
+            image:"./img/samsung.png"
+        },
+        A13:{
+            modelName:"Samsung Galaxy A13",
+            priceInRs:85999,
+            priceIn$:641,
+            batteryCapacity:`(Li-Po Non removable), 5000 mAh -
+             Fast charging 33W, 100% in 65 min (advertised)`, 
+            audio: "Speaker Phone",
+            sizeInInches: 6.5,
+            colors:['Black','White','Blue','Peach'],
+            sim:"Hybrid Dual SIM, Dual Standby, (Nano-SIM)",
+            rating:2.3,
+            image:"./img/samsung.png"
+        },
+        A03:{
+            modelName:"Samsung Galaxy A03",
+            priceInRs:48500,
+            priceIn$:241,
+            batteryCapacity:`(Li-Po Non removable), 5000 mAh  
+            - Fast charging 25W`, 
+            audio: "3.5mm Audio Jack, Speaker Phone",
+            sizeInInches: 6.6,
+            colors:['Black','White','Blue','Peach'],
+            sim:"Hybrid Dual SIM, Dual Standby, (Nano-SIM)",
+            rating:4.5,
+            image:"./img/samsung.png"
+        },
+        S22:{
+            modelName:"Samsung Galaxy S22",
+            priceInRs: 71500,
+            priceIn$:355,
+            batteryCapacity:`(Li-Po Non removable), 5000 mAh  
+            - Fast charging 25W`, 
+            audio: "3.5mm Audio Jack, Speaker Phone",
+            sizeInInches: 6.4,
+            colors:['Black','White','Blue','Peach'],
+            sim:"Hybrid Dual SIM, Dual Standby, (Nano-SIM)",
+            rating:3.2,
+            image:"./img/samsung.png"
+        },
+        M52:{
+            modelName:"Samsung Galaxy M52",
+            priceInRs: 71500,
+            priceIn$:355,
+            batteryCapacity:`(Li-Po Non removable), 5000 mAh  
+            - Fast charging 25W`, 
+            audio: "3.5mm Audio Jack, Speaker Phone",
+            sizeInInches: 6.4,
+            colors:['Black','White','Blue','Peach'],
+            sim:"Hybrid Dual SIM, Dual Standby, (Nano-SIM)",
+            rating:3.2,
+            image:"./img/samsung.png"
+        }
+    },
+    oppo:{
+        F21:{
+            modelName:"Oppo F21 Pro",
+            priceInRs:85999,
+            priceIn$:641,
+            batteryCapacity:`(Li-Po Non removable), 5000 mAh -
+             Fast charging 33W, 100% in 65 min (advertised)`, 
+            audio: "Speaker Phone",
+            sizeInInches: 6.5,
+            colors:['Black','White','Blue','Peach'],
+            sim:"Hybrid Dual SIM, Dual Standby, (Nano-SIM)",
+            rating:2.3,
+            image:"./img/oppo.png"
+        },
+        A96:{
+            modelName:"Oppo A96",
+            priceInRs:48500,
+            priceIn$:241,
+            batteryCapacity:`(Li-Po Non removable), 5000 mAh  
+            - Fast charging 25W`, 
+            audio: "3.5mm Audio Jack, Speaker Phone",
+            sizeInInches: 6.6,
+            colors:['Black','White','Blue','Peach'],
+            sim:"Hybrid Dual SIM, Dual Standby, (Nano-SIM)",
+            rating:4.5,
+            image:"./img/oppo.png"
+        },
+        A16e:{
+            modelName:"Oppo A16e",
+            priceInRs: 71500,
+            priceIn$:355,
+            batteryCapacity:`(Li-Po Non removable), 5000 mAh  
+            - Fast charging 25W`, 
+            audio: "3.5mm Audio Jack, Speaker Phone",
+            sizeInInches: 6.4,
+            colors:['Black','White','Blue','Peach'],
+            sim:"Hybrid Dual SIM, Dual Standby, (Nano-SIM)",
+            rating:3.2,
+            image:"./img/oppo.png"
+        },
+        A76:{
+            modelName:"Oppo A76",
+            priceInRs:85999,
+            priceIn$:641,
+            batteryCapacity:`(Li-Po Non removable), 5000 mAh -
+             Fast charging 33W, 100% in 65 min (advertised)`, 
+            audio: "Speaker Phone",
+            sizeInInches: 6.5,
+            colors:['Black','White','Blue','Peach'],
+            sim:"Hybrid Dual SIM, Dual Standby, (Nano-SIM)",
+            rating:2.3,
+            image:"./img/oppo.png"
+        },
+        A16K:{
+            modelName:"Oppo A16k",
+            priceInRs:48500,
+            priceIn$:241,
+            batteryCapacity:`(Li-Po Non removable), 5000 mAh  
+            - Fast charging 25W`, 
+            audio: "3.5mm Audio Jack, Speaker Phone",
+            sizeInInches: 6.6,
+            colors:['Black','White','Blue','Peach'],
+            sim:"Hybrid Dual SIM, Dual Standby, (Nano-SIM)",
+            rating:4.5,
+            image:"./img/oppo.png"
+        },
+        A15:{
+            modelName:"Oppo A15",
+            priceInRs: 71500,
+            priceIn$:355,
+            batteryCapacity:`(Li-Po Non removable), 5000 mAh  
+            - Fast charging 25W`, 
+            audio: "3.5mm Audio Jack, Speaker Phone",
+            sizeInInches: 6.4,
+            colors:['Black','White','Blue','Peach'],
+            sim:"Hybrid Dual SIM, Dual Standby, (Nano-SIM)",
+            rating:3.2,
+            image:"./img/oppo.png"
+        },
+        A95:{
+            modelName:"Oppo A95",
+            priceInRs: 71500,
+            priceIn$:355,
+            batteryCapacity:`(Li-Po Non removable), 5000 mAh  
+            - Fast charging 25W`, 
+            audio: "3.5mm Audio Jack, Speaker Phone",
+            sizeInInches: 6.4,
+            colors:['Black','White','Blue','Peach'],
+            sim:"Hybrid Dual SIM, Dual Standby, (Nano-SIM)",
+            rating:3.2,
+            image:"./img/oppo.png"
+        }
+    },
+    infinix:{
+        HOT12I:{
+            modelName:"Infinix Hot 12i",
+            priceInRs:85999,
+            priceIn$:641,
+            batteryCapacity:`(Li-Po Non removable), 5000 mAh -
+             Fast charging 33W, 100% in 65 min (advertised)`, 
+            audio: "Speaker Phone",
+            sizeInInches: 6.5,
+            colors:['Black','White','Blue','Peach'],
+            sim:"Hybrid Dual SIM, Dual Standby, (Nano-SIM)",
+            rating:2.3,
+            image:"./img/infinix.png"
+        },
+        SMART6HD:{
+            modelName:"Infinix Smart 6HD",
+            priceInRs:48500,
+            priceIn$:241,
+            batteryCapacity:`(Li-Po Non removable), 5000 mAh  
+            - Fast charging 25W`, 
+            audio: "3.5mm Audio Jack, Speaker Phone",
+            sizeInInches: 6.6,
+            colors:['Black','White','Blue','Peach'],
+            sim:"Hybrid Dual SIM, Dual Standby, (Nano-SIM)",
+            rating:4.5,
+            image:"./img/infinix.png"
+        },
+        HOT11PLAY:{
+            modelName:"Infinix Hot 11Play",
+            priceInRs: 71500,
+            priceIn$:355,
+            batteryCapacity:`(Li-Po Non removable), 5000 mAh  
+            - Fast charging 25W`, 
+            audio: "3.5mm Audio Jack, Speaker Phone",
+            sizeInInches: 6.4,
+            colors:['Black','White','Blue','Peach'],
+            sim:"Hybrid Dual SIM, Dual Standby, (Nano-SIM)",
+            rating:3.2,
+            image:"./img/infinix.png"
+        },
+        ZERO5G:{
+            modelName:"Infinix Zero 5G",
+            priceInRs:85999,
+            priceIn$:641,
+            batteryCapacity:`(Li-Po Non removable), 5000 mAh -
+             Fast charging 33W, 100% in 65 min (advertised)`, 
+            audio: "Speaker Phone",
+            sizeInInches: 6.5,
+            colors:['Black','White','Blue','Peach'],
+            sim:"Hybrid Dual SIM, Dual Standby, (Nano-SIM)",
+            rating:2.3,
+            image:"./img/infinix.png"
+        },
+        NOTE12:{
+            modelName:"Infinix Note 12",
+            priceInRs:48500,
+            priceIn$:241,
+            batteryCapacity:`(Li-Po Non removable), 5000 mAh  
+            - Fast charging 25W`, 
+            audio: "3.5mm Audio Jack, Speaker Phone",
+            sizeInInches: 6.6,
+            colors:['Black','White','Blue','Peach'],
+            sim:"Hybrid Dual SIM, Dual Standby, (Nano-SIM)",
+            rating:4.5,
+            image:"./img/infinix.png"
+        },
+        HOT12:{
+            modelName:"Infinix Hot 12",
+            priceInRs: 71500,
+            priceIn$:355,
+            batteryCapacity:`(Li-Po Non removable), 5000 mAh  
+            - Fast charging 25W`, 
+            audio: "3.5mm Audio Jack, Speaker Phone",
+            sizeInInches: 6.4,
+            colors:['Black','White','Blue','Peach'],
+            sim:"Hybrid Dual SIM, Dual Standby, (Nano-SIM)",
+            rating:3.2,
+            image:"./img/infinix.png"
+        },
+        HOT12PLAY:{
+            modelName:"Infinix Hot 12 Play",
+            priceInRs: 71500,
+            priceIn$:355,
+            batteryCapacity:`(Li-Po Non removable), 5000 mAh  
+            - Fast charging 25W`, 
+            audio: "3.5mm Audio Jack, Speaker Phone",
+            sizeInInches: 6.4,
+            colors:['Black','White','Blue','Peach'],
+            sim:"Hybrid Dual SIM, Dual Standby, (Nano-SIM)",
+            rating:3.2,
+            image:"./img/infinix.png"
+        }
+    },
+    vivo:{
+        X80:{
+            modelName:"Vivo X80",
+            priceInRs:85999,
+            priceIn$:641,
+            batteryCapacity:`(Li-Po Non removable), 5000 mAh -
+             Fast charging 33W, 100% in 65 min (advertised)`, 
+            audio: "Speaker Phone",
+            sizeInInches: 6.5,
+            colors:['Black','White','Blue','Peach'],
+            sim:"Hybrid Dual SIM, Dual Standby, (Nano-SIM)",
+            rating:2.3,
+            image:"./img/vivo.png"
+        },
+        V23:{
+            modelName:"Vivo V23",
+            priceInRs:48500,
+            priceIn$:241,
+            batteryCapacity:`(Li-Po Non removable), 5000 mAh  
+            - Fast charging 25W`, 
+            audio: "3.5mm Audio Jack, Speaker Phone",
+            sizeInInches: 6.6,
+            colors:['Black','White','Blue','Peach'],
+            sim:"Hybrid Dual SIM, Dual Standby, (Nano-SIM)",
+            rating:4.5,
+            image:"./img/vivo.png"
+        },
+        Y01:{
+            modelName:"Vivo Y01",
+            priceInRs: 71500,
+            priceIn$:355,
+            batteryCapacity:`(Li-Po Non removable), 5000 mAh  
+            - Fast charging 25W`, 
+            audio: "3.5mm Audio Jack, Speaker Phone",
+            sizeInInches: 6.4,
+            colors:['Black','White','Blue','Peach'],
+            sim:"Hybrid Dual SIM, Dual Standby, (Nano-SIM)",
+            rating:3.2,
+            image:"./img/vivo.png"
+        },
+        Y21T:{
+            modelName:"Vivo Y21t",
+            priceInRs:85999,
+            priceIn$:641,
+            batteryCapacity:`(Li-Po Non removable), 5000 mAh -
+             Fast charging 33W, 100% in 65 min (advertised)`, 
+            audio: "Speaker Phone",
+            sizeInInches: 6.5,
+            colors:['Black','White','Blue','Peach'],
+            sim:"Hybrid Dual SIM, Dual Standby, (Nano-SIM)",
+            rating:2.3,
+            image:"./img/vivo.png"
+        },
+        Y15S:{
+            modelName:"Vivo Y15S",
+            priceInRs:48500,
+            priceIn$:241,
+            batteryCapacity:`(Li-Po Non removable), 5000 mAh  
+            - Fast charging 25W`, 
+            audio: "3.5mm Audio Jack, Speaker Phone",
+            sizeInInches: 6.6,
+            colors:['Black','White','Blue','Peach'],
+            sim:"Hybrid Dual SIM, Dual Standby, (Nano-SIM)",
+            rating:4.5,
+            image:"./img/vivo.png"
+        },
+        V23E:{
+            modelName:"Vivo V23e",
+            priceInRs: 71500,
+            priceIn$:355,
+            batteryCapacity:`(Li-Po Non removable), 5000 mAh  
+            - Fast charging 25W`, 
+            audio: "3.5mm Audio Jack, Speaker Phone",
+            sizeInInches: 6.4,
+            colors:['Black','White','Blue','Peach'],
+            sim:"Hybrid Dual SIM, Dual Standby, (Nano-SIM)",
+            rating:3.2,
+            image:"./img/vivo.png"
+        },
+        Y33T:{
+            modelName:"Vivo V33T",
+            priceInRs: 71500,
+            priceIn$:355,
+            batteryCapacity:`(Li-Po Non removable), 5000 mAh  
+            - Fast charging 25W`, 
+            audio: "3.5mm Audio Jack, Speaker Phone",
+            sizeInInches: 6.4,
+            colors:['Black','White','Blue','Peach'],
+            sim:"Hybrid Dual SIM, Dual Standby, (Nano-SIM)",
+            rating:3.2,
+            image:"./img/vivo.png"
+        }
+    },
+    tecnospark:{
+        POVANEO:{
+            modelName:"Tecno Pova Neo",
+            priceInRs:85999,
+            priceIn$:641,
+            batteryCapacity:`(Li-Po Non removable), 5000 mAh -
+             Fast charging 33W, 100% in 65 min (advertised)`, 
+            audio: "Speaker Phone",
+            sizeInInches: 6.5,
+            colors:['Black','White','Blue','Peach'],
+            sim:"Hybrid Dual SIM, Dual Standby, (Nano-SIM)",
+            rating:2.3,
+            image:"./img/tecno.png"
+        },
+        POP5LTE:{
+            modelName:"Tecno Pop 5LTE",
+            priceInRs:48500,
+            priceIn$:241,
+            batteryCapacity:`(Li-Po Non removable), 5000 mAh  
+            - Fast charging 25W`, 
+            audio: "3.5mm Audio Jack, Speaker Phone",
+            sizeInInches: 6.6,
+            colors:['Black','White','Blue','Peach'],
+            sim:"Hybrid Dual SIM, Dual Standby, (Nano-SIM)",
+            rating:4.5,
+            image:"./img/tecno.png"
+        },
+        POP5:{
+            modelName:"Tecno Pop 5",
+            priceInRs: 71500,
+            priceIn$:355,
+            batteryCapacity:`(Li-Po Non removable), 5000 mAh  
+            - Fast charging 25W`, 
+            audio: "3.5mm Audio Jack, Speaker Phone",
+            sizeInInches: 6.4,
+            colors:['Black','White','Blue','Peach'],
+            sim:"Hybrid Dual SIM, Dual Standby, (Nano-SIM)",
+            rating:3.2,
+            image:"./img/tecno.png"
+        },
+        CAMON18T:{
+            modelName:"Tecno Camon 18T",
+            priceInRs:85999,
+            priceIn$:641,
+            batteryCapacity:`(Li-Po Non removable), 5000 mAh -
+             Fast charging 33W, 100% in 65 min (advertised)`, 
+            audio: "Speaker Phone",
+            sizeInInches: 6.5,
+            colors:['Black','White','Blue','Peach'],
+            sim:"Hybrid Dual SIM, Dual Standby, (Nano-SIM)",
+            rating:2.3,
+            image:"./img/tecno.png"
+        },
+        CAMON18P:{
+            modelName:"Tecno Camon 18P",
+            priceInRs:48500,
+            priceIn$:241,
+            batteryCapacity:`(Li-Po Non removable), 5000 mAh  
+            - Fast charging 25W`, 
+            audio: "3.5mm Audio Jack, Speaker Phone",
+            sizeInInches: 6.6,
+            colors:['Black','White','Blue','Peach'],
+            sim:"Hybrid Dual SIM, Dual Standby, (Nano-SIM)",
+            rating:4.5,
+            image:"./img/tecno.png"
+        },
+        CAMON18:{
+            modelName:"Tecno Camon 18",
+            priceInRs: 71500,
+            priceIn$:355,
+            batteryCapacity:`(Li-Po Non removable), 5000 mAh  
+            - Fast charging 25W`, 
+            audio: "3.5mm Audio Jack, Speaker Phone",
+            sizeInInches: 6.4,
+            colors:['Black','White','Blue','Peach'],
+            sim:"Hybrid Dual SIM, Dual Standby, (Nano-SIM)",
+            rating:3.2,
+            image:"./img/tecno.png"
+        },
+        SPARK8C:{
+            modelName:"Tecno Spark 8C",
+            priceInRs: 71500,
+            priceIn$:355,
+            batteryCapacity:`(Li-Po Non removable), 5000 mAh  
+            - Fast charging 25W`, 
+            audio: "3.5mm Audio Jack, Speaker Phone",
+            sizeInInches: 6.4,
+            colors:['Black','White','Blue','Peach'],
+            sim:"Hybrid Dual SIM, Dual Standby, (Nano-SIM)",
+            rating:3.2
+        }
+    }
+
+}    
+
+
+
+function myFunction() {
+    let brand = document.getElementById("brand").value.toLowerCase();
+    let model = document.getElementById("model").value.toUpperCase();
+    document.getElementById("brand").value = "";
+    document.getElementById("model").value = "";
+    if(mobiles[brand] !== undefined){
+        if(mobiles[brand][model]){
+            let display = `<div class="main"><div><h1>${mobiles[brand][model].modelName}</h1>
+            <h2>${mobiles[brand][model].rating} Rating</h2>
+            </div>
+            <img  id="myImg" src="${mobiles[brand][model].image}" alt="${mobiles[brand][model].modelName}" width="500" height="500px">
+            </div>
+            <table>
+            <tr><th>Model Name:</th><td>${mobiles[brand][model].modelName}</td></tr>
+            <tr><th>Price:</th><td>Rs.${mobiles[brand][model].priceInRs} - ${mobiles[brand][model].priceIn$}$</td></tr>
+            <tr><th>Battery Capacity:</th><td>${mobiles[brand][model].batteryCapacity}</td></tr>
+            <tr><th>Audio:</th><td>${mobiles[brand][model].audio}</td></tr>
+            <tr><th>Size:</th><td>${mobiles[brand][model].sizeInInches}</td></tr>
+            <tr><th>Sim:</th><td>${mobiles[brand][model].sim}</td></tr>
+            <tr><th>Colors Available:</th><td>${mobiles[brand][model].colors[0]},${mobiles[brand][model].colors[1]},${mobiles[brand][model].colors[2]},${mobiles[brand][model].colors[3]}</td></tr>
+            </table>`
+            var element = document.getElementById("hide");
+            element.style.display = "none";
+            document.getElementById("display").innerHTML = display;
+           
+        }
+        else{
+            let display = `<h1 class="notFound">Data Not Found</h1>`
+            document.getElementById("notFound").innerHTML = display ; 
+        }
+    }
+    else{
+        let display = `<h1 class="notFound">Data Not Found</h1>`
+        document.getElementById("notFound").innerHTML = display ;  
+  }
+}
